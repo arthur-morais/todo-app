7435ad0a1dea31436a4cd01ca55bd85558ecf7b9
Arthur <arthurmoraiis@hotmail.com>
2024-01-15 16:15:49 -0300

    add new textstyles

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -1,3 +1,35 @@
+import 'package:flutter/material.dart';
+
 class AppTextStyles {
   AppTextStyles._();
+
+  static const TextStyle small14w400 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
+
+  static const TextStyle small14w600 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+  );
+
+  static const TextStyle medium24w400 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
+
+  static const TextStyle medium26w700 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Inter',
+  );
+
+  static const TextStyle big34w600 = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+  );
 }

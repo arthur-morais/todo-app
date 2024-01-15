79169148f560b80df65f9345d8c3fe503a48c001
Arthur <arthurmoraiis@hotmail.com>
2024-01-15 16:15:58 -0300

    create custom text form field widget

---
+++ lib/constants/widgets/custom_text_form_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../colors.dart';
+import '../text_styles.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField({
+    super.key,
+    this.controller,
+    this.fieldLabel,
+    this.validator,
+  });
+
+  final TextEditingController? controller;
+  final String? fieldLabel;
+  final FormFieldValidator<String>? validator;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  final defaultBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: const BorderSide(color: AppColors.grey),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      textInputAction: TextInputAction.next,
+      validator: widget.validator,
+      style: AppTextStyles.small14w400.apply(color: AppColors.black70),
+      controller: widget.controller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.white,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        hintText: widget.fieldLabel,
+        hintStyle: AppTextStyles.small14w400.apply(color: AppColors.black60),
+        border: defaultBorder,
+        errorBorder: defaultBorder.copyWith(
+            borderSide: const BorderSide(color: AppColors.redError)),
+        errorStyle: AppTextStyles.small14w400,
+      ),
+    );
+  }
+}

ff2f7067acb4942d5589e514891725b2cd061f35
Arthur <arthurmoraiis@hotmail.com>
2024-01-15 16:16:22 -0300

    create onboarding state file

---
+++ lib/features/onboarding/onboarding_state.dart
@@ -0,0 +1,9 @@
+abstract class OnboardingState {}
+
+class OnboardingInitialState extends OnboardingState {}
+
+class OnboardingLoadingState extends OnboardingState {}
+
+class OnboardingSuccessState extends OnboardingState {}
+
+class OnboardingErrorState extends OnboardingState {}

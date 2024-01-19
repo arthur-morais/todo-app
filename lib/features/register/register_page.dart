95ffb31ebf1763fc4b065cfcd762719d411c8c8f
Arthur <arthurmoraiis@hotmail.com>
2024-01-19 00:02:47 -0300

    add form fields to register page

--- lib/features/register/register_page.dart
+++ lib/features/register/register_page.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/text_styles.dart';
+import 'package:todo_app/constants/validator.dart';
+import 'package:todo_app/constants/widgets/custom_button.dart';
 import 'package:todo_app/constants/widgets/custom_text_form_field.dart';
 
 import '../../constants/colors.dart';
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({super.key});
+  RegisterPage({super.key});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool passwordVisibility = true;
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         leading: Text(
@@ -34,17 +44,54 @@ class _RegisterPageState extends State<RegisterPage> {
           ),
           Column(
             children: [
-              Text(
+              const Text(
                 'create a new account',
                 style: AppTextStyles.medium26w700,
               ),
               Form(
+                  key: _formKey,
                   child: Column(
-                children: [
-                  CustomTextFormField(),
-                  CustomTextFormField(),
-                ],
-              )),
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: screenWidth * 0.4),
+                        child: CustomTextFormField(
+                          controller: widget.emailController,
+                          validator: Validator.validateEmail,
+                          fieldLabel: 'email',
+                          obscureText: false,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: screenWidth * 0.4),
+                        child: CustomTextFormField(
+                          controller: widget.passwordController,
+                          validator: Validator.validatePassword,
+                          fieldLabel: 'password',
+                          obscureText: passwordVisibility,
+                          suffixIcon: IconButton(
+                            icon: passwordVisibility
+                                ? const Icon(Icons.visibility)
+                                : const Icon(Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                passwordVisibility = !passwordVisibility;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: screenWidth * 0.4),
+                        child: CustomButton(
+                          buttonText: 'register',
+                          buttonWidth: screenWidth * 0.6,
+                          buttonHeight: screenHeight * 0.05,
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  )),
             ],
           ),
         ],

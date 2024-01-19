fac889f25ccb855e4894e8d83987e6094451ab50
Arthur <arthurmoraiis@hotmail.com>
2024-01-19 00:01:35 -0300

    add basic, email and password validations

--- lib/constants/validator.dart
+++ lib/constants/validator.dart
@@ -1,3 +1,36 @@
 class Validator {
   Validator._();
+
+  static String? validateBasic(String? value) {
+    if (value != null && value.isEmpty) {
+      return "This field can not be empty.";
+    }
+    return null;
+  }
+
+  static String? validateEmail(String? value) {
+    final condition = RegExp(
+      r"/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g",
+    );
+
+    validateBasic(value);
+
+    if (value != null && !condition.hasMatch(value)) {
+      return "Enter a valid e-mail.";
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    final condition =
+        RegExp(r"/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm");
+
+    validateBasic(value);
+
+    if (value != null && !condition.hasMatch(value)) {
+      return "Enter a valid password.";
+    }
+    return null;
+  }
 }
